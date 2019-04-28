1bbc7f64c868bc107be0960129b2951ffec8a60c
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-28 15:28:52 -0300

    Setting redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+
 import GlobalStyle from './styles/global';
 import Main from './pages/Main';
 
 const App = () => (
-  <Fragment>
+  <Provider store={store}>
     <GlobalStyle />
     <Main />
-  </Fragment>
+  </Provider>
 );
 
 export default App;
