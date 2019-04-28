72f59b04d0ee94c735aed14aa8564e77f13e45b4
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-28 16:33:07 -0300

    Adding favorites action creator and reducer

---
+++ src/store/reducers/favorites.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = [];
+
+export default function favorites(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'ADD_FAVORITE':
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+}
