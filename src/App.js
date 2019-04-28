dc8e693aa864239c8c959ffbb8fc60a5a2eed10d
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-28 15:49:31 -0300

    Setting main route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
-
-import Main from './pages/Main';
+import Routes from './routes';
 
 const App = () => (
   <Provider store={store}>
-    <Main />
+    <Routes />
   </Provider>
 );
 
