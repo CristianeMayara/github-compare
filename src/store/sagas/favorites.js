33c291b919516f5aaef9f8c5b24193dd5b71856f
Cristiane Mayara <cris.mayara@gmail.com>
2019-05-02 21:26:37 -0300

    Treatment for duplicated repo

--- src/store/sagas/favorites.js
+++ src/store/sagas/favorites.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import moment from 'moment';
 
 import api from '../../services/api';
@@ -9,21 +9,27 @@ export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-    const repositoryData = {
-      id: data.id,
-      name: data.name,
-      owner: {
-        login: data.owner.login,
-        avatar_url: data.owner.avatar_url,
-      },
-      stargazers_count: data.stargazers_count,
-      forks_count: data.forks_count,
-      open_issues_count: data.open_issues_count,
-      pushed_at: data.pushed_at,
-      lastCommit: moment(data.pushed_at).fromNow(),
-    };
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
 
-    yield put(addFavoriteSuccess(repositoryData));
+    if (isDuplicated) {
+      yield put(addFavoriteFailure('Repositório duplicado'));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.name,
+        owner: {
+          login: data.owner.login,
+          avatar_url: data.owner.avatar_url,
+        },
+        stargazers_count: data.stargazers_count,
+        forks_count: data.forks_count,
+        open_issues_count: data.open_issues_count,
+        pushed_at: data.pushed_at,
+        lastCommit: moment(data.pushed_at).fromNow(),
+      };
+
+      yield put(addFavoriteSuccess(repositoryData));
+    }
   } catch (err) {
     yield put(addFavoriteFailure('Erro ao adicionar repositório'));
   }
